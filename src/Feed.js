0bcdad0c6249086be6fa01c0307925acaaba9eac
Christian Lewis <christianlewis024@gmail.com>
2020-10-15 09:31:02 -0700

    posts styled for now

--- src/Feed.js
+++ src/Feed.js
@@ -11,7 +11,7 @@ function Feed() {
             <MessageSender />
             <Post
             profilePic="https://pbs.twimg.com/profile_images/1289438305969254402/UBOYNi2s_400x400.jpg"
-            message="Hello friends"
+            message="Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, Hello friends, "
             timestamp="2/2/2020"
             username="Christian L"
             image="https://media.giphy.com/media/pO4UHglOY2vII/giphy.gif"
