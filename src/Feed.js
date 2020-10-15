6e2aab1e06e3cf8e2263b0558ac084960a402b2a
Christian Lewis <christianlewis024@gmail.com>
2020-10-14 21:07:47 -0700

    feed component and css setup

---
+++ src/Feed.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Feed.css"
+
+function Feed() {
+    return (
+        <div className="feed">
+            <h2>i am the feed</h2>
+
+        </div>
+    )
+}
+
+export default Feed 
