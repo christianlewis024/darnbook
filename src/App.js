da82f14b8623238edaffa2936c48c8dc9a82d611
Christian Lewis <christianlewis024@gmail.com>
2020-10-14 20:41:45 -0700

    added header with responsiveness

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header"
 
 function App() {
   return (
-    <div className="app">
-      <h1>Hello friends, and also world.</h1>
+    <div className="app">  
+      <Header/>
     </div>
   );
 }
