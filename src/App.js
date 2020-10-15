06588352d1891bd4ca647286be988109cdf08854
Christian Lewis <christianlewis024@gmail.com>
2020-10-14 17:48:47 -0700

    basic react app setup and cleaned up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello friends, and also world.</h1>
+    </div>
+  );
+}
+
+export default App;
