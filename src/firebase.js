94596163627060e1c624b804eab5ed5b694afbdd
Christian Lewis <christianlewis024@gmail.com>
2020-10-14 17:49:31 -0700

    firebase config created

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAYRYry6GzzKJ49T4vPKHXePeZx4NldmFI",
+    authDomain: "darnbook-f6497.firebaseapp.com",
+    databaseURL: "https://darnbook-f6497.firebaseio.com",
+    projectId: "darnbook-f6497",
+    storageBucket: "darnbook-f6497.appspot.com",
+    messagingSenderId: "587187061851",
+    appId: "1:587187061851:web:3d75679bf017b556bdab7e",
+    measurementId: "G-5QKNCV0DP4"
+  };
